275b482236684ea8cce45e0b23d87f1e8d288979
eaxdev <eaxdev@ya.ru>
2020-06-09 22:13:15 +0300

    cleanup

--- src/test/java/io/github/eaxdev/jsonsql4j/query/SelectTest.java
+++ src/test/java/io/github/eaxdev/jsonsql4j/query/SelectTest.java
@@ -16,7 +16,7 @@ public class SelectTest {
     @DisplayName("Should get select")
     void shouldGetSelect() {
         String json = TestUtil.readFileByPath("select/Select.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT field1, field2 AS test FROM schema.table1", selectQuery.getQuery());
     }
 
@@ -24,7 +24,7 @@ public class SelectTest {
     @DisplayName("Should get select with simple criteria")
     void shouldGetSelectWithSimpleCriteria() {
         String json = TestUtil.readFileByPath("select/SelectWithSimpleCriteria.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT field1, field2 AS test FROM schema.table1 WHERE field3 = 5",
                 selectQuery.getQuery());
     }
@@ -33,7 +33,7 @@ public class SelectTest {
     @DisplayName("Should get select with [and] criteria")
     void shouldGetSelectWithAndCriteria() {
         String json = TestUtil.readFileByPath("select/SelectWithAndCriteria.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT field1, field2 AS test FROM schema.table1 WHERE (field3 = 5 AND field4 = 3)",
                 selectQuery.getQuery());
     }
@@ -42,7 +42,7 @@ public class SelectTest {
     @DisplayName("Should get select with [or] criteria")
     void shouldGetSelectWithOrCriteria() {
         String json = TestUtil.readFileByPath("select/SelectWithOrCriteria.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT field1, field2 AS test FROM schema.table1 WHERE (field3 = 5 OR field4 = 3)",
                 selectQuery.getQuery());
     }
@@ -51,7 +51,7 @@ public class SelectTest {
     @DisplayName("Should get select with join")
     void shouldGetSelectWithJoin() {
         String json = TestUtil.readFileByPath("select/SelectWithJoin.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT field1, field2 AS test FROM schema.table1 INNER JOIN schema2.table2 ON field2 = field3",
                 selectQuery.getQuery());
     }
@@ -60,7 +60,7 @@ public class SelectTest {
     @DisplayName("Should get simple select with join")
     void shouldGetSelectWithExpression() {
         String json = TestUtil.readFileByPath("select/SelectWithExpression.json");
-        SelectQuery selectQuery = new SelectQuery(json);
+        Query selectQuery = new SelectQuery(json);
         assertEquals("SELECT count(*) FROM schema.table1", selectQuery.getQuery());
     }
 }
